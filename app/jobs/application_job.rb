4c4dec8311df3a78fae8e1579f1be7a502b25d13
joseluisrodriguez99 <ragonzalezr@bt.unal.edu.co>
2018-08-09 08:57:45 -0500

    Enviar1

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,11 +1,3 @@
 class ApplicationJob < ActiveJob::Base
-    ##dsdsdsdsdsd
-    ##dsdsdsdsdsd
-    ##dsdsdsdsdsd
-    ##dsdsdsdsdsd
-    ##dsdsdsdsdsd
-    ##dsdsdsdsdsd
-    def
-        ##dsdsdsdsdsd
-    end
+    
 end
